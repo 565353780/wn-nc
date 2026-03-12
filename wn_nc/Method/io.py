46fc4b9302fb3596305d230dfe20faa146a50c4a
chLi <565353780@qq.com>
2026-03-12 15:04:24 +0800

    create sample orient pcd file algo

--- wn_nc/Method/io.py
+++ wn_nc/Method/io.py
@@ -1,7 +1,8 @@
 import os
 import trimesh
 import numpy as np
-from typing import Union
+from typing import Optional, Union
+
 
 def loadPoints(pcd_file_path: str) -> Union[np.ndarray, None]:
     pcd_file_format = os.path.splitext(pcd_file_path)[-1]
@@ -22,3 +23,36 @@ def loadPoints(pcd_file_path: str) -> Union[np.ndarray, None]:
     print('\t pcd_file_path:', pcd_file_path)
 
     return None
+
+def postProcessMesh(mesh: Union[trimesh.Trimesh, trimesh.Scene],
+) -> Optional[trimesh.Trimesh]:
+    if isinstance(mesh, trimesh.Scene):
+        mesh = mesh.to_geometry()
+
+    if not isinstance(mesh, trimesh.Trimesh):
+        print('[ERROR][io::postProcessMesh]')
+        print(f'\t Loaded object is not a Trimesh, got type: {type(mesh)}')
+        return None
+
+    if not hasattr(mesh, 'vertex_normals') or mesh.vertex_normals is None:
+        mesh.vertex_normals = mesh.vertex_normals
+
+    return mesh
+
+def loadMeshFile(
+    mesh_file_path: str,
+) -> Optional[trimesh.Trimesh]:
+    if not os.path.exists(mesh_file_path):
+        print('[ERROR][io::loadMeshFile]')
+        print('\t mesh file not exist!')
+        print('\t mesh_file_path:', mesh_file_path)
+        return None
+
+    mesh = trimesh.load(mesh_file_path, process=False)
+
+    mesh = postProcessMesh(mesh)
+    if mesh is None:
+        print('[ERROR][io::loadMeshFile]')
+        print('\t postProcessMesh failed!')
+
+    return mesh
---
+++ wn_nc/Method/sample.py
@@ -0,0 +1,40 @@
+import os
+import trimesh
+import numpy as np
+
+from wn_nc.Method.io import loadMeshFile
+from wn_nc.Method.path import createFileFolder, removeFile
+
+
+def sampleOrientPoints(
+    mesh: trimesh.Trimesh,
+    sample_point_num: int,
+) -> np.ndarray:
+    samples, face_indices = trimesh.sample.sample_surface(mesh, sample_point_num)
+    normals = mesh.face_normals[face_indices]
+
+    orient_points = np.column_stack((samples, normals))
+    return orient_points
+
+def sampleOrientPcdFile(
+    mesh_file_path: str,
+    sample_point_num: int,
+    save_pcd_file_path: str,
+    overwrite: bool=False,
+) -> bool:
+    if os.path.exists(save_pcd_file_path):
+        if not overwrite:
+            return True
+
+        removeFile(save_pcd_file_path)
+
+    assert os.path.exists(mesh_file_path)
+
+    mesh = loadMeshFile(mesh_file_path)
+
+    orient_points = sampleOrientPoints(mesh, sample_point_num)
+
+    createFileFolder(save_pcd_file_path)
+
+    np.savetxt(save_pcd_file_path, orient_points, fmt='%.6f', delimiter=' ')
+    return True
